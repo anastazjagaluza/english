38e94c8c888dec91d108907ed23376bd17c1edc7
Anastazja Galuza <38051431+anastazjagaluza@users.noreply.github.com>
2021-05-28 11:19:37 +0200

    Delete dist directory

--- dist/sw.js
+++
@@ -1,4 +0,0 @@
-importScripts("precache-manifest.9f325e6fa5a3d488b088ccb0d8f8c0f5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
